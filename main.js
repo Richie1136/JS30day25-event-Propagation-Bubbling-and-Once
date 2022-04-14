0fc0711a6215f9f512516f522e79b6940ec55424
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-04-14 13:56:04 -0400

    Added a once property which will only allow you to click on div once

--- main.js
+++ main.js
@@ -2,11 +2,13 @@ const divs = document.querySelectorAll('div')
 
 function logText(event) {
   console.log(this.classList.value)
+  // event.stopPropagation() // Stop bubbling
   console.log(this)
 }
 
 divs.forEach(div => {
   div.addEventListener('click', logText, {
-    capture: true
+    capture: false,
+    once: true
   })
 })
